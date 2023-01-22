2428ce60a25b5ee85ac055f63e672713d8be874c
Faraz <farazmalik17465@gmail.com>
2023-01-21 19:01:52 -0500

    Chatbox and basic socket.io

--- dispacher-frontend/src/App.js
+++ dispacher-frontend/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Chat from 'react-simple-chat';
+import io from 'socket.io-client'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'react-simple-chat/src/components/index.css';
+import config from './config.json'
+
+
+
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      messages: []
+    }
+  }
+
+  componentDidMount() {
+    this.socket = io(config.endpoint);
+  }
+
+  handleUserSend(m) {
+    this.socket.emit(JSON.stringify(m));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Chat user={{id: 1}}
+        messages={this.state.messages}
+        title="apples"
+        onSend={console.log}
+      />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
