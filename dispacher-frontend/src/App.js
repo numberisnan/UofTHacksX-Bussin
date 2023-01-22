5735c45d0dc83ed7868c1857d156aa13a77b6f17
Faraz <farazmalik17465@gmail.com>
2023-01-21 21:16:23 -0500

    Add socket integration

--- dispacher-frontend/src/App.js
+++ dispacher-frontend/src/App.js
@@ -6,23 +6,48 @@ import 'react-simple-chat/src/components/index.css';
 import config from './config.json'
 
 
-
 class App extends React.Component {
   
   constructor(props) {
     super(props);
 
     this.state = {
-      messages: []
+      messages: [],
+      username: "bus42",
+      userid: 1
     }
+
+    this.handleUserSend = this.handleUserSend.bind(this);
   }
 
   componentDidMount() {
-    this.socket = io(config.endpoint);
+    if (!this.socket) {
+      this.socket = io(config.endpoint);
+      this.socket.on("connect", () => {
+        console.log(this.socket.id);
+
+        this.socket.on("server_chat", chat => {
+          const m = this.state.messages;
+          m.push({
+            id: m.length,
+            text: chat,
+            user: {
+              id: 2
+            }
+          });
+          this.setState({
+            messages: m
+          })
+        });
+      });
+      
+    }
+    
   }
 
   handleUserSend(m) {
-    this.socket.emit(JSON.stringify(m));
+    this.socket.emit("client_chat", m.text);
+    console.log("here");
   }
 
   render() {
@@ -30,8 +55,8 @@ class App extends React.Component {
       <div className="App">
         <Chat user={{id: 1}}
         messages={this.state.messages}
-        title="apples"
-        onSend={console.log}
+        title="Chat"
+        onSend={this.handleUserSend}
       />
       </div>
     );
